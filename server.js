29d8f856b9869c589e724e86bd95293cc02d5fcd
parvatalaprasanth <49365892+parvatalaprasanth@users.noreply.github.com>
2023-06-07 14:00:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,18 @@ const { v4: uuidV4 } = require('uuid')
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
-})
+
 
 app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room })
+})
+
+app.get('/hello', (req, res) => {
+  res.send("hello")
+})
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
 })
 
 io.on('connection', socket => {
